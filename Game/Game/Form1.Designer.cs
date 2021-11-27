42fd729056f05775b5f1faf4233b01d32ba48b00
pilipalaaa <73490496+pilipalaaa@users.noreply.github.com>
2021-11-27 17:52:28 +0800

    Add files via upload

---
+++ Game/Game/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(0, 2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(504, 304);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(521, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(246, 187);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 353);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(307, 27);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(355, 353);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "发送";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(588, 339);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "进入游戏";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(588, 389);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "退出游戏";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(521, 277);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 29);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "播放";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(658, 277);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(94, 29);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "停止";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30000;
+            this.timer1.Tag = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Game/Game/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class Form1 : Form
+    {
+        private NetworkStream stream;
+        private TcpClient tcpClient;
+        SoundPlayer player = new SoundPlayer("D:/VS2019WORK/Game/帝听sakya_千界_千月兔-暖色胶片.wav");
+        //定义发送数据的套接字
+        Socket socket_send;
+        private int flag;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tcpClient = new TcpClient();
+            //套接字建立连接
+
+
+            socket_send = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint point = new IPEndPoint(IPAddress.Parse("10.1.230.74"), 3900);
+            socket_send.Connect(point);
+            try
+            {
+                //向指定的IP地址的服务器发送连接请求
+                tcpClient.Connect("10.1.230.74", 3900);
+                listBox1.Items.Add("连接成功");
+                stream = tcpClient.GetStream();
+                receive_stream();//接收字节流并显示在屏幕上
+
+            }
+            catch
+            {
+                listBox1.Items.Add("服务器未启动");
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (tcpClient.Connected)
+            {
+                string action = textBox1.Text.ToString();
+                listBox1.Items.Add("输入的信息为：" + action);
+                send_stream(action);
+                receive_stream();
+
+            }
+            else
+            {
+                listBox1.Items.Add("连接已断开");
+            }
+        }
+
+
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+                player.Load();
+                player.Play();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        /*
+        * 将字节流用GBK格式编码在listbox里显示
+        */
+        void receive_stream()
+        {
+            byte[] receive_data = new byte[1024];
+            //定义编码格式
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);//为使用GB2312做准备
+            System.Text.Encoding GBK = System.Text.Encoding.GetEncoding("GBK");
+            if (stream.CanRead)
+            {
+
+                int len = stream.Read(receive_data, 0, receive_data.Length);
+                string msg = GBK.GetString(receive_data, 0, receive_data.Length);
+
+                string str = "\r\n";
+                char[] str1 = str.ToCharArray();
+                //乱码集合
+                string[] messy_code = { "??[2J ", "[5m", "[44m", "[37;0m", "[1;33m", "[1;32m", "[1;31m" };
+                string[] msg1 = msg.Split(str1);//以换行符为分隔符
+                for (int j = 0; j < msg1.Length; j++)//逐行显示
+                {
+                    //过滤乱码
+                    msg1[j] = msg1[j].Replace(messy_code[0], " ");
+                    msg1[j] = msg1[j].Replace(messy_code[1], " ");
+                    msg1[j] = msg1[j].Replace(messy_code[2], " ");
+                    msg1[j] = msg1[j].Replace(messy_code[3], " ");
+                    msg1[j] = msg1[j].Replace(messy_code[4], " ");
+                    msg1[j] = msg1[j].Replace(messy_code[5], " ");
+                    msg1[j] = msg1[j].Replace(messy_code[6], " ");
+                    listBox1.Items.Add(msg1[j]);
+                }
+            }
+        }
+        void send_stream(string str)
+        {
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);//为使用GB2312做准备
+            System.Text.Encoding GBK = System.Text.Encoding.GetEncoding("GBK");
+            byte[] buffer = GBK.GetBytes(str + "\n");
+            stream.Write(buffer, 0, buffer.Length);
+        }
+        private void start_game_Click(object sender, EventArgs e)
+        {
+            tcpClient = new TcpClient();
+            //套接字建立连接
+
+
+            socket_send = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint point = new IPEndPoint(IPAddress.Parse("10.1.230.74"), 3900);
+            socket_send.Connect(point);
+            try
+            {
+                //向指定的IP地址的服务器发送连接请求
+                tcpClient.Connect("10.1.230.74", 3900);
+                listBox1.Items.Add("连接成功");
+                stream = tcpClient.GetStream();
+                receive_stream();//接收字节流并显示在屏幕上
+
+            }
+            catch
+            {
+                listBox1.Items.Add("服务器未启动");
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (stream != null)//关闭连接，关闭流
+            {
+                stream.Close();
+                tcpClient.Close();
+                socket_send.Close();
+            }
+            listBox1.Items.Add("已经退出游戏");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+                player.Stop();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Thread th = new Thread(play_pic);
+            th.IsBackground = true;
+            th.Start();
+
+        }
+        void play_pic()
+        {
+            flag++;
+            string picturePath = @"D:\VS2019WORK\Game\pic\" + flag + ".jpg";
+            pictureBox1.Image = Image.FromFile(picturePath);
+            if (flag == 4)
+            {
+                flag = 0;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            flag++;
+            string picturePath = @"D:\VS2019WORK\Game\pic\" + flag + ".jpg";
+            pictureBox1.Image = Image.FromFile(picturePath);
+            if (flag == 4)
+            {
+                flag = 0;
+            }
+        }
+    }
+}
